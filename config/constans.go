1b658fe5846e24cfb1d18fa70ca13f65cb473161
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2021-10-06 19:42:05 +0700

    CHANGE CONSTANS TBL_TRX_KELUARAN_ISB TO TBL_TRX_KELUARAN

--- config/constans.go
+++ config/constans.go
@@ -9,7 +9,7 @@ const (
 	DB_tbl_trx_keluarantogel                string = "db_tot_mmd.tbl_trx_keluarantogel"
 	DB_tbl_trx_keluarantogel_detail         string = "db_tot_mmd.tbl_trx_keluarantogel_detail"
 	DB_VIEW_CLIENT_VIEW_INVOICE_MMD         string = "db_tot_mmd.client_view_invoice_MMD"
-	DB_tbl_trx_keluarantogel_isb            string = "db_tot_isb.tbl_trx_keluarantogel_isb"
-	DB_tbl_trx_keluarantogel_detail_isb     string = "db_tot_isb.tbl_trx_keluarantogel_detail_isb"
+	DB_tbl_trx_keluarantogel_isb            string = "db_tot_isb.tbl_trx_keluarantogel"
+	DB_tbl_trx_keluarantogel_detail_isb     string = "db_tot_isb.tbl_trx_keluarantogel_detail"
 	DB_VIEW_CLIENT_VIEW_INVOICE_ISB         string = "db_tot_isb.client_view_invoice_ISB"
 )
