fd041bb5b9becf71e26b01ecdf77611b7e873819
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-05-16 04:44:40 +0700

    Update Api Money

--- controller/cclientpasaran.go
+++ controller/cclientpasaran.go
@@ -12,6 +12,7 @@ import (
 	"bitbucket.org/isbtotogroup/api_go/helpers"
 	"bitbucket.org/isbtotogroup/api_go/model"
 	"github.com/buger/jsonparser"
+	"github.com/go-resty/resty/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/nleeper/goment"
 )
@@ -296,6 +297,17 @@ type responseredislistinvoicebet struct {
 	Win       int     `json:"win"`
 	Menang    int     `json:"menang"`
 }
+type responseaxios struct {
+	Status          int    `json:"status"`
+	Token           string `json:"token"`
+	Member_company  string `json:"member_company"`
+	Member_username string `json:"member_username"`
+	Member_credit   int    `json:"member_credit"`
+}
+type responseaxioserror struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
 
 var ctx = context.Background()
 
@@ -349,31 +361,8 @@ func Fetch_token(c *fiber.Ctx) error {
 		website_status = "OFFLINE"
 		website_message = "MAINTENANCE<br>START : " + jamstart + "<br>FINISH : " + jamend
 	}
-	member_username := ""
-	member_company := ""
-	member_saldo := 0
-	switch client.Token {
-	case "qC5YmBvXzabGp34jJlKvnC6wCrr3pLCwBzsLoSzl4k=":
-		member_username = "developer"
-		member_company = "MMD"
-		member_saldo = 5000000
-	case "qwertyuiop":
-		member_username = "Jhon Wick"
-		member_company = "MMD"
-		member_saldo = 1000000
-	case "asdfghjkl":
-		member_username = "Edo Febrian"
-		member_company = "MMD"
-		member_saldo = 200000
-	case "1234567890":
-		member_username = "antonnukue"
-		member_company = "NUK"
-		member_saldo = 1000000
-	case "0987654321":
-		member_username = "developernuke"
-		member_company = "NUK"
-		member_saldo = 2000000
-	}
+	member_company, member_username, member_saldo := Fetch_apimoney(client.Token)
+
 	if !flag {
 		result, err := model.Fetch_Setting()
 		if err != nil {
@@ -431,6 +420,22 @@ func Fetch_token(c *fiber.Ctx) error {
 			"member_credit":   member_saldo,
 		})
 	}
+}
+func Fetch_apimoney(token string) (string, string, int) {
+	axios := resty.New()
+	resp, err := axios.R().
+		SetResult(responseaxios{}).
+		SetError(responseaxioserror{}).
+		SetHeader("Content-Type", "application/json").
+		SetBody(map[string]interface{}{
+			"token": token,
+		}).
+		Post("http://128.199.241.112:6061/api/servicetoken")
+	if err != nil {
+		log.Println(err.Error())
+	}
+	result := resp.Result().(*responseaxios)
+	return result.Member_company, result.Member_username, result.Member_credit
 }
 func FetchAll_pasaran(c *fiber.Ctx) error {
 	client := new(entities.Controller_clientInit)
@@ -1636,59 +1641,4 @@ func _domainsecurity(nmdomain string) bool {
 		}
 	}
 	return flag
-}
-func _deleteredisclient(company, idtrxkeluaran, username, pasarancode, pasaranperiode string) {
-	field_redis := "listinvoice_" + strings.ToLower(company) + "_" + idtrxkeluaran + "_" + strings.ToLower(username)
-	val := helpers.DeleteRedis(field_redis)
-	log.Printf("DELETE REDIS INVOICE %d\n", val)
-	val_limit := helpers.DeleteRedis(
-		fieldlimit_redis + strings.ToLower(company) + "_" +
-			strings.ToLower(username) + "_" +
-			strings.ToLower(pasarancode) + "_" +
-			strings.ToLower(pasaranperiode))
-	log.Printf("DELETE REDIS LIMIT %d\n", val_limit)
-
-	//AGEN
-	val_agen := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran)
-	val_agenlistmember := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTMEMBER")
-	val_agenlistbettable := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTBETTABLE")
-	val_agen4d := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_4D")
-	val_agen3d := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_3D")
-	val_agen2d := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_2D")
-	val_agen2dd := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_2DD")
-	val_agen2dt := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_2DT")
-	val_agencolokbebas := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_COLOK_BEBAS")
-	val_agencolokmacau := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_COLOK_MACAU")
-	val_agencoloknaga := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_COLOK_NAGA")
-	val_agencolokjitu := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_COLOK_JITU")
-	val_agen5050umum := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_50_50_UMUM")
-	val_agen5050special := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_50_50_SPECIAL")
-	val_agen5050kombinasi := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_50_50_KOMBINASI")
-	val_agenmacaukombinasi := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_MACAU_KOMBINASI")
-	val_agendasar := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_DASAR")
-	val_agenshio := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTPERMAINAN_SHIO")
-	val_agenall := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTBET_all")
-	val_agenwinner := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTBET_winner")
-	val_agencancel := helpers.DeleteRedis("LISTPERIODE_AGENT_" + strings.ToLower(company) + "_INVOICE_" + idtrxkeluaran + "_LISTBET_cancel")
-	log.Printf("DELETE REDIS AGEN PERIODE %d\n", val_agen)
-	log.Printf("DELETE REDIS AGEN PERIODE LISTMEMBER %d\n", val_agenlistmember)
-	log.Printf("DELETE REDIS AGEN PERIODE LISTBETTABEL %d\n", val_agenlistbettable)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN 4D %d\n", val_agen4d)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN 3D %d\n", val_agen3d)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN 2D %d\n", val_agen2d)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN 2DD %d\n", val_agen2dd)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN 2DT %d\n", val_agen2dt)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN COLOK BEBAS %d\n", val_agencolokbebas)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN COLOK MACAU %d\n", val_agencolokmacau)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN COLOK NAGA %d\n", val_agencoloknaga)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN COLOK JITU %d\n", val_agencolokjitu)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN 5050UMUM %d\n", val_agen5050umum)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN 5050SPECIAL %d\n", val_agen5050special)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN 5050KOMBINASI %d\n", val_agen5050kombinasi)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN MACAU KOMBINASI %d\n", val_agenmacaukombinasi)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN DASAR %d\n", val_agendasar)
-	log.Printf("DELETE REDIS AGEN PERIODE PERMAINAN SHIO %d\n", val_agenshio)
-	log.Printf("DELETE REDIS AGEN PERIODE LIST BET ALL %d\n", val_agenall)
-	log.Printf("DELETE REDIS AGEN PERIODE LIST BET WINNER %d\n", val_agenwinner)
-	log.Printf("DELETE REDIS AGEN PERIODE LIST BET CANCEL %d\n", val_agencancel)
 }
